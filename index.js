5598dc7261e6d8425a26bd018dfde12098b39fb2
RJP2024 <ruth.jeanphilippe@gmail.com>
2024-08-31 16:17:13 -0400

    Added js and css

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generateAffirmation(event) {
+  event.preventDefault();
+
+  new Typewriter("#affirmation", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let affirmationormElement = document.querySelector("#affirmation-generator-form");
+affirmationFormElement.addEventListener("submit", generateAffirmation);
